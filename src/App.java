94926dad4a22a2651cd950b3f48b9e05730ffbdc
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-21 03:04:08 +0300

    v2: add developer work details

--- src/App.java
+++ src/App.java
@@ -2,6 +2,11 @@ public class App {
     interface Worker{
         void work();
     }
+    static class Developer implements Worker{
+        public void work(){
+            System.out.println("writing code");
+        }
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
