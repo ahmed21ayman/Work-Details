d1630fcab7347053abc344ede64bc9926e3d3e19
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-21 03:00:17 +0300

    v1: build interface for work

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
 public class App {
+    interface Worker{
+        void work();
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
