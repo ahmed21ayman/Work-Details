0b6c6e523b4a149586ac0d945f786f51fd00e04e
Ahmed Ayman Khalil <ahmedaymen10111@gmail.com>
2026-06-21 03:07:17 +0300

    v3: add tester work details

--- src/App.java
+++ src/App.java
@@ -7,6 +7,13 @@ public class App {
             System.out.println("writing code");
         }
     }
+
+    static class Tester implements Worker{
+        public void work(){
+            System.out.println("test dev work");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
